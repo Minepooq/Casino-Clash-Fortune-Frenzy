df2f70c0be6f6c76751cef1805d110522edaba61
Minepooq <ethanqiu9090@gmail.com>
2024-05-20 19:37:30 -0700

    mainmenutweak

---
+++ Assets/SCRIPTS/mainmenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
+
+public class mainmenu : MonoBehaviour
+{
+    private int quit = 0;
+    public GameObject exit;
+    // Start is called before the first frame update
+    public void playgame()
+    {
+        if(quit == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+    }
+    
+
+    public void quitgame()
+    {
+        if(quit == 1)
+        {
+            Application.Quit();
+            Debug.Log("quitgame");
+        }
+        
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (quit == 0)
+            {
+
+                Debug.Log("quit game?");
+                quit = 1;
+                exit.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("no quit game");
+                quit = 0;
+                exit.SetActive(false);
+            }
+
+        }
+    }
+    public void exitexit()
+    {
+        quit = 0;
+        exit.SetActive(false);
+    }
+
+}
